dbc43508e5f44b737206431546ddaf347fd3aa8f
Muzafar Ali <muzfr7@gmail.com>
2021-06-28 07:48:32 +0500

    read file flag from command line

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
 func main() {
-	//
+	file := flag.String("file", "", "-file paths.csv")
+	flag.Parse()
+	if *file == "" {
+		fmt.Println("file parameter is required.")
+		os.Exit(1)
+	}
 }
