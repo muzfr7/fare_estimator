6f04b71241048201e2e3596f1ef9d1c33eb5ac53
Muzafar Ali <muzfr7@gmail.com>
2021-06-28 08:05:43 +0500

    add method to path domain to identify day and night time

--- app/domain/path/path.go
+++ app/domain/path/path.go
@@ -1,8 +1,21 @@
 package path
 
+import "time"
+
 // Path represents a position in a ride.
 type Path struct {
 	Latitude  float64 `json:"lat"`
 	Longitude float64 `json:"lng"`
 	Timestamp int32   `json:"timestamp"`
+}
+
+// IsDayTime checks whether its a day time.
+func (p *Path) IsDayTime() bool {
+	t := time.Unix(int64(p.Timestamp), 0).UTC()
+	hour := t.Hour()
+	if hour >= 5 && hour < 24 {
+		return true
+	}
+
+	return false
 }
