9f24309fb4a4f2d02a3bf74477987ff1236c1e44
Muzafar Ali <muzfr7@gmail.com>
2021-06-28 08:14:09 +0500

    add a method to cmd/cli to handle fare estimation from csv

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -4,6 +4,10 @@ import (
 	"flag"
 	"fmt"
 	"os"
+
+	appCSV "github.com/muzfr7/fare_estimator/app/infrastructure/filesystem/csv"
+	fareUsecase "github.com/muzfr7/fare_estimator/app/usecases/fare"
+	rideUsecase "github.com/muzfr7/fare_estimator/app/usecases/ride"
 )
 
 func main() {
@@ -13,6 +17,37 @@ func main() {
 		fmt.Println("file parameter is required.")
 		os.Exit(1)
 	}
+}
+
+func handler(file string, csvReader appCSV.Reader, csvWriter appCSV.Writer, rideSVC rideUsecase.Service, fareSVC fareUsecase.Service) error {
+	// read each row of csv file in rowChan
+	rowChan, rowChanErr, err := csvReader.Read(file)
+	if err != nil {
+		return fmt.Errorf("failed to read file; error: %v", err)
+	}
+
+	// create ride.Ride from rowChan and return it in rideChan
+	rideChan, rideChanErr := rideSVC.Create(rowChan)
+
+	// estimate fare from rideChan for each ride and return it in fareChan
+	fareChan := fareSVC.Estimate(rideChan)
+
+	// create a new csv file with estimated ride fares from fareChan
+	done, writeChanErr, err := csvWriter.Write(fareChan)
+	if err != nil {
+		return fmt.Errorf("failed to initialize write channel; error: %v", err)
+	}
+	select {
+	case <-done:
+	case err := <-mergeErrorChannels(rowChanErr, rideChanErr, writeChanErr):
+		if err != nil {
+			return fmt.Errorf("failed to perform estimation; received an error from channels; error: %v", err)
+		}
+	}
+
+	return nil
+}
+
 func mergeErrorChannels(channels ...<-chan error) <-chan error {
 	errChan := make(chan error)
 
